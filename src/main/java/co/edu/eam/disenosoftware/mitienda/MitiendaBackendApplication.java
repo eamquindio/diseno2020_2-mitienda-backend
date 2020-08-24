e2e791e894f8b31daf016ae4b2b97b6a6eda4cb3
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo-2.local>
2020-08-24 17:00:16 -0500

    project init

---
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/MitiendaBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.eam.disenosoftware.mitienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitiendaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitiendaBackendApplication.class, args);
+	}
+
+}
