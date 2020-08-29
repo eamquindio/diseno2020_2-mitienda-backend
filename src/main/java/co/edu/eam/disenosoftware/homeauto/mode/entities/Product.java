ea396fde7385f0b289d35a8eb310a3b8a1040693
ErrorHyperion <juan.villanueva.1447@eam.edu.co>
2020-08-29 18:54:25 -0500

    Correccion de errores de codificacion y adicion de comentarios

--- src/main/java/co/edu/eam/disenosoftware/homeauto/mode/entities/Product.java
+++ src/main/java/co/edu/eam/disenosoftware/homeauto/mode/entities/Product.java
@@ -7,16 +7,31 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "products")
+/**
+ * Product's class
+ */
 public class Product implements Serializable {
 
+  /**
+   * Product's constructor
+   */
   public Product() {
   }
 
+  /**
+   * Product's Primary key
+   */
   @Id
   private String id;
 
+  /**
+   * Product's image
+   */
   private String image;
 
+  /**
+   * Product's name
+   */
   private String name;
 
 }
--- src/main/java/co/edu/eam/disenosoftware/homeauto/mode/entities/Store.java
+++ src/main/java/co/edu/eam/disenosoftware/homeauto/mode/entities/Store.java
@@ -6,33 +6,70 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 
+
 @Entity
 @Table(name = "stores")
+/**
+ *Stores class
+ */
 public class Store implements Serializable {
 
+  /**
+   *Store's constructor
+   */
   public Store() {
   }
 
+  /**
+   *Store's primary key
+   */
   @Id
   private String id;
 
+  /**
+   *Store's name
+   */
   private String name;
 
+  /**
+   *Store's owner
+   */
   private String owner;
 
+  /**
+   *Store's address
+   */
   private String address;
 
+  /**
+   *Store's image
+   */
   private String image;
 
+  /**
+   *Store's delivery
+   */
   private boolean delivery;
 
+  /**
+   *Store's open status
+   */
   @Column(name = "is_open")
   private boolean isOpen;
 
+  /**
+   *Store's phone
+   */
   private String phone;
 
+  /**
+   *Store's email
+   */
   private String email;
 
-  private String Password;
+  /**
+   *Store's password
+   */
+  private String password;
 
 }
