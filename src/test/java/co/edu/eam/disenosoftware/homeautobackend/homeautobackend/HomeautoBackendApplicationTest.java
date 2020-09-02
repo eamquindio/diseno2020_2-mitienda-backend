5be86f6517b944bdb2be013d137e5341cf1bc19f
ErrorHyperion <juan.villanueva.1447@eam.edu.co>
2020-09-01 22:43:35 -0500

    corrigiendo name con mala sintaxis en shoppingcart

--- src/test/java/co/edu/eam/disenosoftware/homeautobackend/homeautobackend/HomeautoBackendApplicationTest.java
+++ src/test/java/co/edu/eam/disenosoftware/homeautobackend/homeautobackend/HomeautoBackendApplicationTest.java
@@ -1,4 +1,4 @@
-package co.edu.eam.disenosoftware.homeautobackend;
+package co.edu.eam.disenosoftware.homeautobackend.homeautobackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
