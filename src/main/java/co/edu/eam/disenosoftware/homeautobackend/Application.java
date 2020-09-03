07bb676676ad5b5eee0f2865e5a01a6c6c015a9c
Jeronimo90-eng <jeronimo.robledo.4436@eam.edu.co>
2020-09-03 09:14:43 -0500

    Agregando entidades

--- src/main/java/co/edu/eam/disenosoftware/homeautobackend/Application.java
+++ src/main/java/co/edu/eam/disenosoftware/homeautobackend/Application.java
@@ -1,5 +1,6 @@
 package co.edu.eam.disenosoftware.homeautobackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Runner class.
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
   /**
    * Main method.
    *
