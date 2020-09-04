c37061358b9110429ced8fd8b7f30cfe48166899
GilberttValentine <pablo.guzman.4983@eam.edu.co>
2020-09-04 14:10:06 -0500

    Re organizando entidades en la carpeta correspondiente

--- src/main/java/co/edu/eam/disenosoftware/homeautobackend/model/entities/ShoppingCartProduct.java
+++ src/main/java/co/edu/eam/disenosoftware/homeautobackend/model/entities/ShoppingCartProduct.java
@@ -4,10 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * Shopping Cart's Product class
