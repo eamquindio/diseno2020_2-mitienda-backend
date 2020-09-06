926e95ec68603f12acd1ed4e1ac53b38b029c214
ErrorHyperion <juan.villanueva.1447@eam.edu.co>
2020-09-05 20:09:44 -0500

    Adding query to find the list of products of a store with the id given

--- src/main/java/co/edu/eam/disenosoftware/homeautobackend/repositories/ProductStoreRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/homeautobackend/repositories/ProductStoreRepository.java
@@ -1,10 +1,13 @@
 package co.edu.eam.disenosoftware.homeautobackend.repositories;
 
+import co.edu.eam.disenosoftware.homeautobackend.model.entities.ProductStore;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * product store Repository
@@ -18,4 +21,17 @@ public class ProductStoreRepository {
    */
   @PersistenceContext
   private EntityManager em;
+
+  /**
+   *Find and return the products of the store that has the id sent
+   * @param id , Foreign key
+   * @return List of Products of the Store with the id that has been sent
+   */
+  public List<ProductStore> getAllProductsOfStore(Long id) {
+    String queryStr = "SELECT ps FROM ProductStore ps WHERE ps.store.id = :id";
+    Query query = em.createQuery(queryStr);
+    query.setParameter("id", id);
+    return query.getResultList();
+  }
+
 }
