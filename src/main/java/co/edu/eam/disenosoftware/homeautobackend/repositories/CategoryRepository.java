f837538e2d50c7d4c150dfa85ee12b20b1c1dc5d
JoseHurtadoVGitHub <jose.hurtado.9321@eam.edu.com>
2020-09-05 23:35:56 -0500

    Agregando Listado De Categorias de Tienda

--- src/main/java/co/edu/eam/disenosoftware/homeautobackend/repositories/CategoryRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/homeautobackend/repositories/CategoryRepository.java
@@ -1,10 +1,13 @@
 package co.edu.eam.disenosoftware.homeautobackend.repositories;
 
+import co.edu.eam.disenosoftware.homeautobackend.model.entities.Category;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * Category Repository
@@ -18,4 +21,26 @@ public class CategoryRepository {
    */
   @PersistenceContext
   private EntityManager em;
+
+
+  /**
+   * Method to Find Id categories from Store
+   * @param id Primary Key Category
+   * @return List Of Category Store
+   */
+  public List<Category> getCategoryOfStore(Long id) {
+
+    String quertyStr = "SELECT category FROM Category WHERE category.store.id = :id";
+
+    Query query = em.createQuery(quertyStr);
+
+    query.setParameter("id", id);
+
+    return query.getResultList();
+  }
+
+
+
+
+
 }
