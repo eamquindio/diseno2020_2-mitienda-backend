2a5a14f78f17c4b640dd5d2fea4be3b50fc23709
GilberttValentine <pablo.guzman.4983@eam.edu.co>
2020-09-10 15:38:03 -0500

    Adding CRUD Stores and Query from current Orders

--- src/main/java/co/edu/eam/disenosoftware/homeautobackend/repositories/StoreRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/homeautobackend/repositories/StoreRepository.java
@@ -23,6 +23,42 @@ public class StoreRepository {
   private EntityManager em;
 
   /**
+   * Add a Store
+   * @param store Store to create
+   */
+  public void create(Store store) {
+    em.persist(store);
+  }
+
+  /**
+   * Find a Store by primary key
+   * @param id Primary key
+   * @return a object Store or null if no exists
+   */
+  public Store find(Long id) {
+    return em.find(Store.class, id);
+  }
+
+  /**
+   * Edit a Store
+   * @param store Store to edit
+   */
+  public void edit(Store store) {
+    em.merge(store);
+  }
+
+  /**
+   * Delete a Store finding it
+   * @param id primary key
+   * @return Store deleted or null if not exist
+   */
+  public Store delete(Long id) {
+    Store store = find(id);
+    em.remove(store);
+    return store;
+  }
+
+  /**
    * A get all stores function
    *
    * @return , return all stores in data base
