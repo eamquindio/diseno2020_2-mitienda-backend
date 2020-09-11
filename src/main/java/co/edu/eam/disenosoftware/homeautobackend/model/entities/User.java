37f8951725f3a3b6e7ea1cdfaa89ab205e54b780
saralucia1224 <sara.carmona.7927@eam.edu.co>
2020-09-11 12:57:30 -0500

    repositorioSara

--- src/main/java/co/edu/eam/disenosoftware/homeautobackend/model/entities/User.java
+++ src/main/java/co/edu/eam/disenosoftware/homeautobackend/model/entities/User.java
@@ -48,4 +48,122 @@ public class User implements Serializable {
    */
   public User() {
   }
+
+  /**
+   * User's constructor
+   * @param id primary key
+   * @param username username to user
+   */
+  public User(Long id, String username) {
+    this.id = id;
+    this.username = username;
+  }
+
+  /**
+   * get id
+   * @return id
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * set id
+   * @param id primary key
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * get Username
+   * @return username
+   */
+  public String getUsername() {
+    return username;
+  }
+
+  /**
+   * set Username
+   * @param username username to setUsername
+   */
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  /**
+   * Get phone
+   * @return phone
+   */
+  public String getPhone() {
+    return phone;
+  }
+
+  /**
+   * Set phone
+   * @param phone phone to setPhone
+   */
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  /**
+   * Get email
+   * @return email
+   */
+  public String getEmail() {
+    return email;
+  }
+
+  /**
+   * Set email
+   * @param email email to setEmail
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  /**
+   * Get password
+   * @return pasword
+   */
+  public String getPassword() {
+    return password;
+  }
+
+  /**
+   * Set password
+   * @param password password to setPassword
+   */
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  /**
+   * Get name
+   * @return name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Set name
+   * @param name name to setName
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Override
+   * @return user
+   */
+  @Override
+  public String toString() {
+    return "User{"
+            + "id=" + id
+            + ", username='" + username + '\''
+            + '}';
+  }
 }
