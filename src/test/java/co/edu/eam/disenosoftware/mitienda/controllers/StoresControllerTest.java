8d25aa263997b8c52585738b5d2efe32f65bf2c5
saralucia1224 <sara.carmona.7927@eam.edu.co>
2020-10-24 14:13:13 -0500

    Organizando error

--- src/test/java/co/edu/eam/disenosoftware/mitienda/controllers/StoresControllerTest.java
+++ src/test/java/co/edu/eam/disenosoftware/mitienda/controllers/StoresControllerTest.java
@@ -1,10 +1,10 @@
 package co.edu.eam.disenosoftware.mitienda.controllers;
 
 import co.edu.eam.disenosoftware.mitienda.model.entities.Category;
-import co.edu.eam.disenosoftware.mitienda.model.entities.Store;
-import co.edu.eam.disenosoftware.mitienda.repositories.StoreRepository;
 import co.edu.eam.disenosoftware.mitienda.model.entities.Order;
+import co.edu.eam.disenosoftware.mitienda.model.entities.Store;
 import co.edu.eam.disenosoftware.mitienda.repositories.OrderRepository;
+import co.edu.eam.disenosoftware.mitienda.repositories.StoreRepository;
 import co.edu.eam.disenosoftware.mitienda.services.OrderService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
@@ -12,21 +12,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -90,7 +85,7 @@ public class StoresControllerTest {
   @Sql("/testdata/controlles_get_all_category_store_by_store_id.sql")
   public void getAllCategoryByStoreId() throws Exception{
 
-    RequestBuilder request = MockMvcRequestBuilders.get("/api/stores/stores/3/categories");
+    RequestBuilder request = MockMvcRequestBuilders.get("/api/stores/3/categories");
 
     ResultActions result = mockMvc.perform((request));
 
