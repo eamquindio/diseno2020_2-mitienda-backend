fc65a30d680d758e055373b9191db71ca9a9061b
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo-2.local>
2020-10-27 07:34:37 -0500

    corrigiendo cobertura

---
+++ backup_db/categories_202010270730.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.categories (icon,"name",id_store) VALUES 
+('1_large.png','Postres, dips y Compotas',1)
+,('2_category.png','Arroz, granos y pastas',2)
+,('3_category.png','Cereales y Granolas',3)
+,('4_category.png','Café, chocolates e infusiones',5)
+,('5_category.png','Azúcar, panela y endulzante',6)
+,('6_category.png','Harinas y mezclas para preparar',4)
+,('7_category.png','Enlatados y conservas',7)
+,('8_category.png','Aceites y vinagres',8)
+,('9_category.png','Salsas, condimentos y sopas',9)
+;
---
+++ backup_db/stores_202010270730.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.stores (address,delivery,email,image,is_open,"name","owner","password",phone) VALUES 
+('Carrera 25 Numero 20-16 Grieta Del invocador',true,'almuercito@gmail.com','2_store.jpg',true,'almuercitos punto com','Manuelito Gamer Yt','c6cff595a0603c1cabf3c563298d0d97','32567451')
+,('Cr 14 #15-10',true,'tucerealito@gmail.com','3_store.jpg',true,'Tu Cerealito ','Juan Manuel Villanueva','b13a71270996109b13a58bd55c638f54','4396113')
+,('Carrera 10 Número 15-24 Ciudad de Demacia',true,'ricospanques@gmail.com','1_large.jpg',true,'Ricos Panqueques','Jose Luis Hurtado Villa','90e528618534d005b1a7e7b7a367813f','1234567')
+,('Dirección de la tienda',true,'tiendaGilbert@gmail.com','store5.jpg',true,'Tienda 5','Pablo Andres','401cec94d3ed586d8cb895c10c0f7db6','3000000000')
+,('calle 11 # 10-31',true,'filotienda@gmail.com','store6.jpg',true,'Filo tienda','Filo','5fbf56b67c14c637fc24dcf623ee42d0','3002020')
+,('calle 12 # 14-32',true,'tiendascj@gmail.com','4_store.jpg',true,'tiendas cj','Sara Lucia','827ccb0eea8a706c4c34a16891f84e7b','123445')
+,('Calle 15 # 15-35',true,'tusponques@gmail.com','8_store.jpg',true,'Cerditos y Gorditos','Cristhian Alvarado','62236d96a215a8424f0115710d2632c7','7747518')
+,('Calle 13 # 20-09',true,'tiendaeng@gmail.com','7_storeng.jpg',true,'TiendaEng','Jeronimo Robledo','e8a48653851e28c69d0506508fb27fc5','541269')
+,('Cr 15 #19-21',true,'minimarket@gmail.com','9_store.jpg',true,'Mini Market La 21','Sebastián Obando Galindo','02236d96a225a8424f0115710d2f32c7','3212876500')
+;
